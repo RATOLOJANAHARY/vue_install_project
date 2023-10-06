3691fa8ef0f0a71361e2864607fae90cb6296863
RATOLOJANAHARY <rjandrejean@gmail.com>
2023-10-06 15:11:13 +0200

    creation interface sur vue

---
+++ vue-project/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="hello">Hello<span class="child-hello">word</span></div>
+</template>
+
+<style lang="scss">
+    @import './assets/style.scss';
+</style>
